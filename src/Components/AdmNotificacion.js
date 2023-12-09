1896e1cd2b48c7a43f518177a4466131c374632a
YsusOP <jesusespitia003@gmail.com>
2023-12-09 10:29:09 -0500

    actualizacion

--- src/Components/AdmNotificacion.js
+++ src/Components/AdmNotificacion.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../Stiles/AdmNotificacion.css';
 import campana from '../IMG/campana.png';
-<<<<<<< HEAD
 import AdmHeader from './AdmHeader';
 
 export default function AdmNotificacion(){
@@ -52,23 +51,5 @@ export default function AdmNotificacion(){
             </article>
         </>
         
-=======
-export default function AdmNotificacion(){
-    return(
-        <article id="Admnotificacioncuerpo">
-                <div class="Admnotificacioncontenedorp">
-                    <header id="Admnotificaciontexto">
-                        <h3>Crear Nueva Notificacion<img src={campana}  alt="image" width="33%"/></h3>
-                    </header>
-                    <p><b>Nombre de la Notificacion</b></p>
-                    <input className="AdmnotificacionNombre" type="text" placeholder="Nombre"/>
-                    <p><b>Fecha de la Notificacion</b></p>
-                    <input className="AdmnotificacionNombre" type="date" placeholder="Fecha"/>
-                    <p><b>Mensaje</b></p>
-                    <input className="Admnotificaciondescripcion" type="text" placeholder="Descripcion"/>
-                    <input type="button" id="Admnotificacionregistrar" value="Enviar notificacion"/>
-                </div>
-            </article>
->>>>>>> ysus
     )
 }
--- src/Components/AdmRutina.js
+++ src/Components/AdmRutina.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../Stiles/AdmRutina.css';
 import Rutina from '../IMG/Rutina.png';
-<<<<<<< HEAD
 import AdmHeader from './AdmHeader';
 export default function AdmRutina(){
     const handleSubmit = async (event) => {
@@ -68,29 +67,5 @@ export default function AdmRutina(){
             </article>
         </>
         
-=======
-export default function AdmRutina(){
-    return(
-        <article id="Admrutinacuerpo">
-                <div id="Admrutinaregistro">
-                    <header id="Admrutinatexto">
-                        <h3>Agregar Rutina<img src={Rutina} alt="image" width="33%"/></h3>
-                    </header>
-                    <p>Nombre Ejercicio</p>
-                    <input type="text" className="Admrutinatexto" placeholder="Nombre ejercicio" required/>
-                    <p>Descripcion del ejercicio</p>
-                    <input type="text" className="Admrutinadescripcion"  placeholder="Descripcion" required/>
-                    <p>Duracion</p>
-                    <input type="text" className="Admrutinatexto" placeholder="Duracion"required/>
-                    <p>Series</p>
-                    <input type="text" className="Admrutinatexto" placeholder="Series"required/>
-                    <p>Repeticiones por Serie </p>
-                    <input type="text" className="Admrutinatexto" placeholder="Repeticiones por serie"required/>
-                    <p>Inserte las imagenes</p>
-                    <input type="file" className="Admrutinaimagenes"/>
-                    <input type="button" id="Admrutinaregistrar" value="Agregar Rutina"/>
-                </div>
-            </article>
->>>>>>> ysus
     )
 }
