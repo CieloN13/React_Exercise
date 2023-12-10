c76361e6a5a1caceaff13901862166903054c830
YsusOP <jesusespitia003@gmail.com>
2023-12-10 15:41:24 -0500

    actualizacion

--- src/Components/AtencionC.js
+++ src/Components/AtencionC.js
@@ -5,18 +5,92 @@ export default function Recomendaciones(){
     return(
         <>
         <Header/>
-         <article id="cuerpoAtencionC">
-            <h1>Atencion al cliente</h1>
-            <h2>(Quejas, reclamos y/o sugerencias)</h2>
-            <h3>Si desea comunicarse con el asesor y necesita preguntar algo referente a la pagina web puede escribirlo en el siguiente cuadro</h3>
-
-            <div className="chat">xd</div>
-            <section>
-                <input type="text" id="Barra"/>
-                <input type="submit" value="Enviar" className="enviar"/>
+        <article id="cuerpoAtencionC">
+            <div>
+                <section>
+                <h1>
+                Consulta con un profesional de la salud
+                </h1>
+                <p>
+                Antes de comenzar cualquier programa de ejercicios, es crucial hablar con un médico o fisioterapeuta para obtener recomendaciones específicas según tu discapacidad y condición física.
+                </p>
             </section>
-
-            <h5>En un momento se comunicaran con ud</h5>
+            <section>
+                <h1>
+                Encuentra un entrenador especializado
+                </h1>
+                <p>
+                Busca un entrenador o instructor con experiencia en adaptaciones para personas con discapacidades. Pueden ayudarte a modificar ejercicios y crear un programa seguro y efectivo.
+                </p>
+            </section>
+            <section>
+                <h1>
+                Elige el tipo de ejercicio adecuado
+                </h1>
+                <p>
+                Hay una amplia gama de actividades físicas adaptadas disponibles, como natación, yoga, tai chi, ciclismo adaptado, pilates y ejercicios de resistencia con bandas elásticas. Elige actividades que se ajusten a tus habilidades y necesidades.
+                </p>
+            </section>
+            <section>
+                <h1>
+                Adapta los ejercicios según sea necesario
+                </h1>
+                <p>
+                Modifica los ejercicios estándar para adaptarlos a tu capacidad. Por ejemplo, puedes utilizar equipos de apoyo como bastones, sillas o ayudas para mantener el equilibrio según sea necesario.
+                </p>
+            </section>
+            <section>
+                <h1>
+                Calentamiento y enfriamiento
+                </h1>
+                <p>
+                No te saltes el calentamiento y el enfriamiento. Estos son pasos importantes para preparar y recuperar tus músculos. El calentamiento puede incluir estiramientos suaves y movimientos para aumentar la circulación sanguínea.
+                </p>
+            </section>
+            </div>
+            <div>
+                <section>
+                <h1>
+                Escucha a tu cuerpo
+                </h1>
+                <p>
+                Reconoce tus límites y no te fuerces más allá de lo que es seguro y cómodo para ti. Aprende a distinguir entre el dolor normal del ejercicio y cualquier sensación que pueda indicar una lesión.
+                </p>
+            </section>
+            <section>
+                <h1>
+                Establece metas realistas
+                </h1>
+                <p>
+                Fija metas alcanzables y progresivas. Celebrar tus logros, por pequeños que sean, te motivará a seguir adelante.
+                </p>
+            </section>
+            <section>
+                <h1>
+                No te desanimes
+                </h1>
+                <p>
+                El progreso puede ser gradual, pero cada paso cuenta. Mantén una actitud positiva y persevera en tu rutina de ejercicio.
+                </p>
+            </section>
+            <section>
+                <h1>
+                Busca apoyo
+                </h1>
+                <p>
+                Participa en comunidades en línea o grupos locales donde puedas conectarte con personas que enfrentan desafíos similares. Compartir experiencias y consejos puede ser muy útil.
+                </p>
+            </section>
+            <section>
+                <h1>
+                Cuida tu nutrición y descanso
+                </h1>
+                <p>
+                Una dieta balanceada y descanso adecuado son fundamentales para cualquier programa de ejercicio. Asegúrate de mantener un equilibrio saludable en tu estilo de vida.
+                </p>
+            </section>
+            </div>
+            
         </article>
         <Piedepagina/>
         </>
